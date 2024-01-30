c0054f113293bef6abe2d9b7a5e495957b0ec464
Hansu Malviya <malviyahansu91191@gmail.com>
2024-01-30 18:23:06 +0530

    u

---
+++ 100daysofcode/sigma web development/0javasciptfull/jsshardha/variables and datatypes/object.js
@@ -0,0 +1,6 @@
+const student = {
+    fullName:"hansu",
+    age:20,
+    gpa:10,
+    isPass:true,
+};
---
+++ 100daysofcode/sigma web development/0javasciptfull/jsshardha/variables and datatypes/script.js
@@ -0,0 +1,54 @@
+console.log("welcome to apna college");
+console.log("The Program works");
+
+name="hansu  malviya";
+console.log("name");
+age=24;
+price=99.99;
+radius=14;
+x=null;   //null  matlab pata hi nhi hai yeh kya hai
+y=undefined; // pata hai kya hai but value khali hai
+
+fullName=25;
+console.log(fullName);
+
+// = to sign means assignment operator matlab asssign kr rha hai value ko
+
+//variable rules
+// case sensitive, only letter underscore, dolllar sign are allowed
+//first charcater ya toh underscore , ya ek letter , ya dollar
+//  Reserved woords are not allowed
+
+// let const and var
+//var can be redeclared and updated... global scope
+//let cannot be redeclared nut can be updated.... block scope
+//const cannot be updated or redeclared .... block scope
+
+
+//data types in js
+
+//number data type
+int ages=24;
+let price=10.5;
+console.log("price");
+
+
+//string data type
+name="hansu";
+console.log("name ");
+
+//boolean data type
+isFollow=true;
+typeof isFollow;
+
+//undefined
+let y;
+typeof y;
+
+//null
+let x = null;
+typeof x;
+
+
+
+
