518610d3c17bda4314161a40278f0cd55e3d2eb5
Hansu Malviya <malviyahansu91191@gmail.com>
2024-02-03 00:12:43 +0530

    arrowFunction updated in js by sharadha mam

---
+++ 100daysofcode/sigma web development/0javasciptfull/jsshardha/5 functions and arrays/script.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    console.log("my name is hansu");
+    console.log("hi this sis hansu");
+}
+
+myFunction();
+myFunction();
+
+
+function myMesssage(msg){
+    //parameter -> input
+    console.log(msg);
+}
+
+myMesssage("i love js");  //argument
